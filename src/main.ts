e693b57997fe1767e5b2bbdda8404165be5e5768
Sardorbek <sardorbekmusilman@gmail.com>
2026-01-16 16:49:02 +0500

    s

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://104.131.48.1/'],
+    origin: ['http://localhost:5173', 'http://104.131.48.1'],
   });
 
   await app.listen(process.env.PORT ?? 3000);
